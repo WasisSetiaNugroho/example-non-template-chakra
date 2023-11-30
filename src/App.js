040dbabaec2cb81f8d24570b3bcc86b3a86e004c
Wasis Setia Nugroho <147807141+WasisSetiaNugroho@users.noreply.github.com>
2023-11-30 14:27:22 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import codemasters from "../src/codemasters.png";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="body">
+          <img src={codemasters} alt="codemasters" className="Logo" />
+          <p className="signin">SIGN IN</p>
+          <p className="text">
+            {" "}
+            Enter your credentials to access your account{" "}
+          </p>
+          <form className="form">
+            <label className="label">
+              Email
+              <input
+                type="email"
+                name="email"
+                className="input"
+                placeholder="Enter your email"
+              />
+            </label>
+            <label className="label">
+              Password
+              <input
+                type="password"
+                name="password"
+                className="input"
+                placeholder="Enter your password"
+              />
+            </label>
+            <label>
+              <input type="submit" className="submit" />
+            </label>
+          </form>
+          <p className="reset">
+            {" "}
+            Forgot your password?
+            <a href="www.codemasters.id" className="link">
+              Reset Password
+            </a>
+          </p>
+        </div>
       </header>
     </div>
   );
